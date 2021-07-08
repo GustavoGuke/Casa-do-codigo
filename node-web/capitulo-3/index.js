430536e8813d209209c6f70450251dc0c48038d5
gusta <gustavo.athenas13@gmail.com>
2021-07-07 23:18:18 -0300

    Servidor com express

---
+++ node-web/capitulo-3/index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+
+const app = express()
+const PORT = 3000
+
+app.get('/',(req,res) => {
+    res.type('text/plain')
+    res.status(200)
+    res.send('Servidor com express')
+})
+
+app.use((req,res) => {
+    res.type('text/plain')
+    res.status(404)
+
+    res.send('404 - NOT FOUND')
+})
+
+app.use((err,req,res,next) => {
+    console.log(err)
+    res.type('text/plain')
+    res.status(500)
+    res.send("500 - SERVER ERROR")
+})
+
+app.listen(PORT, () => console.log('Server ok na porta'+PORT))
